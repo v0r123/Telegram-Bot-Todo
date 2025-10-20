025fa2203d60f0980b55fe226eebc637fcf3165a
work <work@work.work>
2025-10-20 21:17:15 +0300

    Fix text utils

--- src/utils/telegram/text/index.ts
+++ src/utils/telegram/text/index.ts
@@ -1,10 +1,29 @@
-export const formatText = async (text: string, callbacks: ((args?: any) => any)[]) => {
+type FormatCallback = () => Promise<string>;
+
+export const formatText = async (text: string, callbacks: FormatCallback[]) => {
+
+    if (typeof text !== 'string') {
+        throw new TypeError('The "text" parameter must be a string');
+    }
+
+    if (!Array.isArray(callbacks)) {
+        throw new TypeError('The "callbacks" parameter must be an array');
+    }
+
+    if (!/{(\d+)}/.test(text)) {
+        return text;
+    }
 
     const results = await Promise.all(callbacks.map(cb => cb()));
+    const maxIndex = results.length - 1;
 
-    const formattedText = text.replace(/{(\d+)}/g, (match, number) => {
+    return text.replace(/{(\d+)}/g, (match, number) => {
+        const index = Number(number);
+        if (index > maxIndex) {
+            throw new Error(`Placeholder index ${index} exceeds available results`);
+        }
+
         return typeof results[number] !== 'undefined' ? results[number] : match;
     });
 
-    return formattedText;
 }
